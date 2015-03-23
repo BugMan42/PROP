09a3ed68ceb2b6fd8b77d975d382d059b7df6f46
BugMan42 <guillem812@gmail.com>
2015-03-23 12:20:35 +0100

    3 packages puestos

---
+++ src/Persistencia/ControladorPersistencia.java
@@ -0,0 +1,7 @@
+package Persistencia;
+
+/**
+ * Created by bug on 23/03/15.
+ */
+public class ControladorPersistencia {
+}
---
+++ src/Presentacio/ControladorPresentacio.java
@@ -0,0 +1,8 @@
+package Presentacio;
+
+/**
+ * Created by bug on 23/03/15.
+ */
+public class ControladorPresentacio {
+
+}
