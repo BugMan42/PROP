a5e5fc7e1d91a50f59f64790a25d062ae789497a
BugMan42 <guillem812@gmail.com>
2015-03-25 13:35:33 +0100

    generador grafo por controladorgrafo

---
+++ src/Domini/ControladorGrafo.java
@@ -0,0 +1,21 @@
+package Domini;
+
+/**
+ * Created by bug on 23/03/15.
+ */
+public class ControladorGrafo {
+    //private Grafo G;
+    public ControladorGrafo() {
+
+    }
+    public void GenerarGrafo() {
+
+        //while all relaciones
+        //add relacion grafo
+
+    }
+   /* public Grafo obt_Grafo() {
+        return G;
+    }*/
+
+}
