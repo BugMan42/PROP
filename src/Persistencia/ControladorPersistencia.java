10e1bf2f2a7485c23646ba593e848d76b3ac5db8
madsam753 <sam92.sp@gmail.com>
2015-03-28 19:33:03 +0100

    ControladorPersistencia y Driver (falta mucho(o todo))

--- src/Persistencia/ControladorPersistencia.java
+++ src/Persistencia/ControladorPersistencia.java
@@ -1,7 +1,30 @@
 package Persistencia;
 
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 /**
  * Created by bug on 23/03/15.
  */
 public class ControladorPersistencia {
+
+    public ControladorPersistencia(){
+
+    }
+
+    public void guardar(String path, String data) throws IOException {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
+            oos.writeObject(data);
+            oos.close();
+        }
+        catch (FileNotFoundException e){
+            throw new IOException("Se ha producido un error al crear el fichero.");
+        }
+        catch (SecurityException e){
+            throw new IOException("El fichero está bloqueado.");
+        }
+    }
 }
