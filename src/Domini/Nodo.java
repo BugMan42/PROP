f96469362aaa982f31d9893ab64875968ce53ebd
BugMan42 <guillem812@gmail.com>
2015-04-01 13:57:13 +0200

    1a implementacion nodo y arista, faltan cosas

---
+++ src/Domini/Nodo.java
@@ -0,0 +1,19 @@
+package Domini;
+
+/**
+ * Created by bug on 1/04/15.
+ */
+public class Nodo {
+    private Congresista C;
+    //Algo mas????
+
+    public Nodo(Congresista A) {
+        C = A;
+    }
+    public Congresista obt_Congresista() {
+        return C;
+    }
+    public void mod_Congresista(Congresista A) {
+        C = A;
+    }
+}
