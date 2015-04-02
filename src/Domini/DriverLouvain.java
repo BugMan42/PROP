b70bebaf32c14bd6755a4aca1ad71e1f46e3f6e7
jose <sam92.sp@gmail.com>
2015-04-02 20:40:56 +0200

    Louvain

--- src/Domini/DriverLouvain.java
+++ src/Domini/DriverLouvain.java
@@ -5,7 +5,15 @@ package Domini;
  */
 public class DriverLouvain {
     public static void main(String[] args){
+        Congresista c1 = new Congresista("12345678A Paco Sobaco 38 Springfield Massachusets Republicano");
+        Congresista c2 = new Congresista("12345678B Barack Obama 53 Springfield Massachusets Republicano");
+        Nodo n1 = new Nodo(c1);
+        Nodo n2 = new Nodo(c2);
+        Arista a = new Arista(n1, n2, 3);
+
+        Grafo g = new Grafo();
+
         Louvain l = new Louvain();
-        l.ejecutar_algoritmo();
+        l.ejecutar_algoritmo(g);
     }
 }
