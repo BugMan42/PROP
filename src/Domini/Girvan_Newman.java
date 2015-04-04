f1d509ceedd28344c168a9433a20f85edfa0311f
FALC <falc@null.net>
2015-04-04 21:33:59 +0200

    Algoritmo Girvan-Newman (Work In Progress)

--- src/Domini/Girvan_Newman.java
+++ src/Domini/Girvan_Newman.java
@@ -1,5 +1,8 @@
 package Domini;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by falc on 2/04/15.
  */
@@ -11,8 +14,25 @@ public class Girvan_Newman extends Algoritmo{
         double[][] a = g.obt_lista_ady();
         boolean done = false;
         while (!done) {
+
             for (int i = 0; i < N; ++i) {
-                //BFS + 'betweenness'
+
+                boolean[] visited = new boolean[N];
+                double[][] bfsmatrix = new double[N][N];
+                double[] node_weigth = new double[N];
+                double[] node_distance = new double[N];
+                LinkedList<Integer> q = new LinkedList<Integer>();
+
+                q.addLast(i);
+                node_distance[i] = 0;
+                node_weigth[i] = 1;
+
+                while(!q.isEmpty())
+                {
+                    int v = q.removeFirst();
+                    //para nodos adyacentes a v
+                }
+
             }
 
             //eliminar nodo con más 'betweenness'
