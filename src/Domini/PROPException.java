77ed0a378a5a5a9b4aa5c3361ce8dc8761a82a31
mcdinero <camilo.andres.castro@est.fib.upc.edu>
2015-04-13 22:32:43 +0200

    Creacion clase de las excepciones a peticion de rosa

--- src/Domini/PROPException.java
+++ src/Domini/PROPException.java
@@ -3,9 +3,8 @@ package Domini;
 /**
  * Created by usuario on 13/04/2015.
  */
-public class PROPException extends Exception{
+public class PROPException extends Exception {
+    public PROPException() {
 
-    public PROPException(String m) {
-        super(m);
     }
 }
