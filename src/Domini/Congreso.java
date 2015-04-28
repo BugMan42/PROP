f4779a65e4e5c1f77fb7a0f60e619118d3318007
BugMan42 <guillem812@gmail.com>
2015-04-28 15:45:27 +0200

    media adaptacion congreso con tst

--- src/Domini/Congreso.java
+++ src/Domini/Congreso.java
@@ -2,115 +2,73 @@ package Domini;
 
 import java.util.*;
 
-/**
- * Created by bug on 20/03/15.
- */
 public class Congreso {
     static final String error1 = "Congreso no contiene el dni";
     static final String error2 = "Congreso ya contiene el dni";
     static final String error3 = "DNI NO VALIDO";
-    private ArrayList<Congresista> Lista;
+    private TST<Congresista> tst;
     //private int N;
     public Congreso() {
-        Lista = new ArrayList<Congresista>();
+        tst = new TST<Congresista>();
         //N = 0;
     }
-    private int BuscarIndice(Dni dni) {
-        //si no se encuentra retorna -1
-        int n = Lista.size();
-        boolean found = false;
-        int i = 0;
-        while (!found && i < n) {
-            if (Lista.get(i).obtDni().equals(dni)) found = true;
-            else ++i;
-        }
-        if (!found) i = -1;
-        return i;
+    public int size() {
+        return tst.size();
     }
-    public void addCongresista(Congresista C) {
-        if (!contieneCongresista(C.obtDni())) {
-            Lista.add(C);
-        }
-        else throw new IllegalArgumentException(error2);
-        //++N;
+    public void addCongresista(Congresista C) throws Exception {
+        tst.insertar(C.obtDni().toString(),C);
     }
     public void eliminarCongresista(Congresista C) {
-        Lista.remove(C);
-        //--N;
+        tst.borrar(C.obtDni().toString());
     }
-    public ArrayList<Congresista> obtener_lista() {
-        return Lista;
+    public ArrayList<String> obtener_lista() {
+        return tst.consultarClaves();
     }
-    /*public boolean contieneCongresista(Congresista C) {
-        //post: retorna verdadero si el congresista esta en el congreso
-        return Lista.contains(C);
-    }*/
     public boolean contieneCongresista(Dni dni) {
-        //post: retorna verdadero si el dni esta en el congreso
-        int n = Lista.size();
-        int i = 0;
-        while (i < n) {
-            if (Lista.get(i).obtDni().equals(dni)) return true;
-            else ++i;
-        }
-        return false;
+        return tst.existe(dni.toString());
     }
     public void EliminarCongresista(Dni dni) {
-            int i = BuscarIndice(dni);
-            if (i != -1) Lista.remove(i);
-            else throw new IllegalArgumentException(error1);
+        tst.borrar(dni.toString());
     }
     public void EliminarCongreso() {
-        Lista.clear();
+        tst.vaciar();
     }
 
     //####################################
     //########MODIFICADORAS###############
     //####################################
-    public void ModificarDniCongresista(Dni dni,int edad) {
-        int i = BuscarIndice(dni);
-        if (i != -1) Lista.get(i).modEdad(edad);
-        else throw new IllegalArgumentException(error1);
+    public void ModificarDniCongresista(Dni dni,int edad) throws Exception {
+        tst.obtener(dni.toString()).modEdad(edad);
     }
-    public void ModificarNombreCongresista(Dni dni,int nombre) {
-        int i = BuscarIndice(dni);
-        if (i != -1) Lista.get(i).modEdad(nombre);
-        else throw new IllegalArgumentException(error1);
+    public void ModificarNombreCongresista(Dni dni,String nombre) throws Exception {
+        tst.obtener(dni.toString()).modNombre(nombre);
     }
-    public void ModificarApellidoCongresista(Dni dni,int apellido) {
-        int i = BuscarIndice(dni);
-        if (i != -1) Lista.get(i).modEdad(apellido);
-        else throw new IllegalArgumentException(error1);
+    public void ModificarApellidoCongresista(Dni dni,String apellido) throws Exception {
+        tst.obtener(dni.toString()).modApellido(apellido);
     }
-    public void ModificarEdadCongresista(Dni dni,int edad) {
-        int i = BuscarIndice(dni);
-        if (i != -1) Lista.get(i).modEdad(edad);
-        else throw new IllegalArgumentException(error1);
+    public void ModificarEdadCongresista(Dni dni,int edad) throws Exception {
+        tst.obtener(dni.toString()).modEdad(edad);
     }
-    public void ModificarCiudadCongresista(Dni dni,int ciudad) {
-        int i = BuscarIndice(dni);
-        if (i != -1) Lista.get(i).modEdad(ciudad);
-        else throw new IllegalArgumentException(error1);
+    public void ModificarCiudadCongresista(Dni dni,String ciudad) throws Exception {
+        tst.obtener(dni.toString()).modCiudad(ciudad);
     }
-    public void ModificarEstadoCongresista(Dni dni,int estado) {
-        int i = BuscarIndice(dni);
-        if (i != -1) Lista.get(i).modEdad(estado);
-        else throw new IllegalArgumentException(error1);
+    public void ModificarEstadoCongresista(Dni dni,String estado) throws Exception {
+        tst.obtener(dni.toString()).modEstado(estado);
     }
-    public void ModificarPartidoCongresista(Dni dni,int partido) {
-        int i = BuscarIndice(dni);
-        if (i != -1) Lista.get(i).modEdad(partido);
-        else throw new IllegalArgumentException(error1);
+    public void ModificarPartidoCongresista(Dni dni,String partido) throws Exception {
+        tst.obtener(dni.toString()).modPartido(partido);
+    }
+    /** Bueno hay algo mejor????? */
+    public void ModificarDni(Dni dni, Dni dniNuevo) throws Exception {
+        tst.obtener(dni.toString()).modDni(dniNuevo);
+        tst.modificar(dni.toString(),dniNuevo.toString());
     }
     public void ModificarCongresista(Dni dni,Dni dni2 , String nombre, String apellido, int edad, String ciudad, String estado, String partido) {
-        int i = BuscarIndice(dni);
-        if (i != -1) Lista.get(i).modEdad(edad);
-        else throw new IllegalArgumentException(error1);
+
     }
-    public Congresista ConsultarCongresista(Dni dni) {
-        int i = BuscarIndice(dni);
-        if (i != -1) return Lista.get(i);
-        else throw new IllegalArgumentException(error1);
+    public Congresista ConsultarCongresista(Dni dni) throws Exception {
+        Congresista aux = new Congresista(tst.obtener(dni.toString()));
+        return aux;
     }
 
 
