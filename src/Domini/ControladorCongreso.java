7ee32295f8d66ef2a778259fdec16aa3f697580e
jose <sam92.sp@gmail.com>
2015-05-01 20:53:48 +0200

    ControladorCongreso y DriverControladorCongreso (va a petar mucho todo)

--- src/Domini/ControladorCongreso.java
+++ src/Domini/ControladorCongreso.java
@@ -1,5 +1,7 @@
 package Domini;
 
+import java.util.ArrayList;
+
 /**
  * Created by bug on 8/04/15.
  */
@@ -11,15 +13,90 @@ public class ControladorCongreso {
         c = new Congreso();
     }
 
-    public void add_congresista(String s){
+    public int size(){
+        return c.size();
+    }
+
+    public void agregarCongresista(String s) throws Exception {
         String[] aux = s.split("\\s");
-        //for (int i=0; i<7; ++i) System.out.println(aux[i]);
-        //Congresista con = new Congresista(aux[0],aux[1],aux[2],Integer.valueOf(aux[3]),aux[4],aux[5],aux[6]);
-        /*if (c.contieneCongresista(con)) System.out.println("ya esta en el congreso");
-        else System.out.println("no esta en el congreso");
-        if (c.contieneCongresista(aux[0])) System.out.println("ya esta en el congreso");
-        else System.out.println("no esta en el congreso");*/
-        //c.addCongresista(con);
-        //System.out.println(String.valueOf(c.obtener_lista().size()));
+        Dni dni = new Dni(aux[0]);
+        Congresista con = new Congresista(dni,aux[1],aux[2],Integer.valueOf(aux[3]),aux[4],aux[5],aux[6]);
+        c.agregarCongresista(con);
+    }
+
+    public ArrayList<String> obtenerLista(){
+        return c.obtenerLista();
+    }
+
+    public boolean contieneCongresista(String s) throws Exception {
+        Dni dni = new Dni(s);
+        return c.contieneCongresista(dni);
+    }
+
+    public void eliminarCongresista(String s) throws Exception {
+        Dni dni = new Dni(s);
+        c.eliminarCongresista(dni);
+    }
+    public void eliminarCongreso() {
+        c.eliminarCongreso();
+    }
+
+    public void modificarNombreCongresista(String s) throws Exception {
+        String[] aux = s.split("\\s");
+        Dni dni = new Dni(aux[0]);
+        c.modificarNombreCongresista(dni, aux[1]);
+    }
+
+    public void modificarApellidoCongresista(String s) throws Exception {
+        String[] aux = s.split("\\s");
+        Dni dni = new Dni(aux[0]);
+        c.modificarApellidoCongresista(dni,aux[1]);
+    }
+
+    public void modificarEdadCongresista(String s) throws Exception {
+        String[] aux = s.split("\\s");
+        Dni dni = new Dni(aux[0]);
+        c.modificarEdadCongresista(dni, Integer.parseInt(aux[1]));
+    }
+
+    public void modificarCiudadCongresista(String s) throws Exception {
+        String[] aux = s.split("\\s");
+        Dni dni = new Dni(aux[0]);
+        c.modificarCiudadCongresista(dni, aux[1]);
+    }
+
+    public void modificarEstadoCongresista(String s) throws Exception {
+        String[] aux = s.split("\\s");
+        Dni dni = new Dni(aux[0]);
+        c.modificarEstadoCongresista(dni, aux[1]);
+    }
+
+    public void modificarPartidoCongresista(String s) throws Exception {
+        String[] aux = s.split("\\s");
+        Dni dni = new Dni(aux[0]);
+        c.modificarPartidoCongresista(dni, aux[1]);
+    }
+
+    public void modificarDni(String s) throws Exception {
+        String[] aux = s.split("\\s");
+        Dni dni = new Dni(aux[0]);
+        Dni dni_nuevo = new Dni(aux[1]);
+        c.modificarDni(dni, dni_nuevo);
+    }
+
+    public void modificarCongresista(String s) throws  Exception{
+        String[] aux = s.split("\\s");
+        Dni dni = new Dni(aux[0]);
+        Dni dni_nuevo = new Dni(aux[1]);
+        c.modificarCongresista(dni, dni_nuevo, aux[2], aux[3], Integer.parseInt(aux[4]), aux[5], aux[6], aux[7]);
+    }
+
+    public Congresista consultarCongresista(String s) throws Exception {
+        Dni dni = new Dni(s);
+        return c.consultarCongresista(dni);
+    }
+
+    public String toString() {
+        return c.toString();
     }
 }
--- src/Domini/DriverControladorCongreso.java
+++ src/Domini/DriverControladorCongreso.java
@@ -1,46 +1,134 @@
 package Domini;
 
-import java.text.ParseException;
 import java.util.Scanner;
 
 /**
  * Created by Jose on 15/04/2015.
  */
 public class DriverControladorCongreso {
+    // Mensajes excepciones.
+    static final String E1 = "Número de parámetros incorrecto.";
 
     private static ControladorCongreso cc;
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
+        DriverControladorCongreso dcc = new DriverControladorCongreso();
         cc = new ControladorCongreso();
         Scanner ui = new Scanner(System.in);
         int op;
         do {
-            menu();
+            dcc.menu();
             String linea = ui.nextLine();
             String[] params = linea.split("\\s");
-            op = Integer.parseInt(params[0]);
-            switch (op){
-                case 1: add_congresista(params);
+            try {
+                op = Integer.parseInt(params[0]);
+                String aux = "";
+                switch (op) {
+                    case 0:
+                        dcc.print(String.valueOf(cc.size()));
                         break;
+                    case 1:
+                        aux = dcc.validar_num_params(params,8);
+                        cc.agregarCongresista(aux);
+                        break;
+                    case 2:
+                        dcc.print(cc.obtenerLista().toString());
+                        break;
+                    case 3:
+                        aux = dcc.validar_num_params(params, 2);
+                        dcc.print(Boolean.toString(cc.contieneCongresista(aux)));
+                        break;
+                    case 4:
+                        aux = dcc.validar_num_params(params,2);
+                        cc.eliminarCongresista(aux);
+                        break;
+                    case 5:
+                        cc.eliminarCongreso();
+                        break;
+                    case 6:
+                        aux = dcc.validar_num_params(params,3);
+                        cc.modificarNombreCongresista(aux);
+                        break;
+                    case 7:
+                        aux = dcc.validar_num_params(params,3);
+                        cc.modificarApellidoCongresista(aux);
+                        break;
+                    case 8:
+                        aux = dcc.validar_num_params(params,3);
+                        cc.modificarEdadCongresista(aux);
+                        break;
+                    case 9:
+                        aux = dcc.validar_num_params(params,3);
+                        cc.modificarCiudadCongresista(aux);
+                        break;
+                    case 10:
+                        aux = dcc.validar_num_params(params,3);
+                        cc.modificarEstadoCongresista(aux);
+                        break;
+                    case 11:
+                        aux = dcc.validar_num_params(params,3);
+                        cc.modificarPartidoCongresista(aux);
+                        break;
+                    case 12:
+                        aux = dcc.validar_num_params(params,3);
+                        cc.modificarDni(aux);
+                        break;
+                    case 13:
+                        aux = dcc.validar_num_params(params,9);
+                        cc.modificarCongresista(aux);
+                        break;
+                    case 14:
+                        aux = dcc.validar_num_params(params,2);
+                        dcc.print(cc.consultarCongresista(aux).toString());
+                        break;
+                    case 15:
+                        dcc.print(cc.toString());
+                        break;
+                }
+            }
+            catch (Exception e) {
+                dcc.print(e.getMessage());
+                op = -1;
             }
         }
-        while(op != 0);
+        while(op != 16);
     }
 
-    private static void menu(){
-        print("DRIVER DE CONTROLADOR CONGRESO");
-        print("1 add_congresista(String dni, String nombre, String apellido, int edad, String ciudad, String estado, String partido)");
-        print("0 Salir");
+    private void menu(){
+        print("\nDRIVER DE CONTROLADOR CONGRESO");
+        print("0 size()");
+        print("1 agregarCongresista(String dni, String nombre, String apellido, int edad, " +
+                "String ciudad, String estado, String partido)");
+        print("2 obtenerLista()");
+        print("3 contieneCongresista(String dni)");
+        print("4 eliminarCongresista(String dni)");
+        print("5 eliminarCongreso()");
+        print("6 modificarNombreCongresista(Dni dni,String nombre)");
+        print("7 modificarApellidoCongresista(Dni dni,String apellido)");
+        print("8 modificarEdadCongresista(Dni dni,int edad)");
+        print("9 modificarCiudadCongresista(Dni dni,String ciudad)");
+        print("10 modificarEstadoCongresista(Dni dni,String estado)");
+        print("11 modificarPartidoCongresista(Dni dni,String partido)");
+        print("12 modificarDni(Dni dni, Dni dniNuevo)");
+        print("13 modificarCongresista(Dni dni,Dni dniNuevo , String nombre, String apellido," +
+                " int edad, String ciudad, String estado, String partido)");
+        print("14 consultarCongresista(Dni dni)");
+        print("15 toString()");
+        print("16 Salir\n");
     }
 
-    private static void add_congresista(String[] params){
-        String aux = params[1];
-        for (int i=2; i<8; ++i) aux += " " + params[i];
-        cc.add_congresista(aux);
-        //print("Añadido correctamente");
-    }
-
-    private static void print(String s){
+    private void print(String s){
         System.out.println(s);
     }
+
+    public String validar_num_params(String[] params, int num) throws Exception {
+        String aux = "";
+        if (params.length == num){
+            aux = params[1];
+            for (int i = 2; i < num; ++i) aux += " " + params[i];
+            return aux;
+        }
+        else throw new Exception(E1);
+    }
+
 }
