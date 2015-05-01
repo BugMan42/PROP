700769e4e83a61cee73fe7b47f5fe7bada8e4390
BugMan42 <guillem812@gmail.com>
2015-05-01 14:14:36 +0200

    progreso eliminar vertice

--- src/Domini/DriverGrafo.java
+++ src/Domini/DriverGrafo.java
@@ -22,34 +22,31 @@ public class DriverGrafo {
         print(a.consultarObjetos()+"");
         print(b.consultarObjetos()+"");
         */
-        PriorityQueue<Integer> a = new PriorityQueue<Integer>();
-        a.add(3);
-        a.add(2);
-        a.add(1);
-        print(a.poll() + "");
-        print(a.poll()+"");
-        print(a.poll()+"");
-
-        /*
         Grafo a = new Grafo();
-        a.agregarVertice("abc");
-        a.agregarVertice("abd");
         a.agregarVertice("a");
         a.agregarVertice("b");
-        //print(a.consultarVertices().toString());
-        //print(a.consultarVerticesID().toString());
+        print(a.consultarVertices().toString());
+        print(a.consultarVerticesID().toString());
+        //a.eliminarVertice(1);
+
+        //print(a.f("h")+" "+a.fPrima(a.f("h")));
         a.agregarArista(0, 1, 2.0);
         a.agregarArista(1, 0, 2.0);
         a.agregarArista(0, 1, 3.0);
         a.agregarArista(1, 0, 3.0);
+        a.eliminarVertice(1);
+        print("Nodos Entrada 1 " + a.nodosEntrada(0));
+        print("Nodos Salida 0 "+a.nodosSalida(0));
+       // print("Nodos Entrada 0 "+a.nodosEntrada(0));
+        //print("Nodos Salida 1 "+a.nodosSalida(1));
         //a.agregarArista(0, 1, 4.0);
         // a.agregarArista(0, 3,1.0);
         //a.agregarArista(0, 2, 1.0);
-        print(a.nodosEntrada(1)+"");
-        print(a.nodosSalida(0) + "");
+       // print(a.nodosEntrada(1)+"");
+        //print(a.nodosSalida(0) + "");
 
-        print(a.pesoAristasVertices(0, 1) + "");
-        print(a.pesoAristasVertices(1, 0) + "");
+        //print(a.pesoAristasVertices(0, 1) + "");
+        //print(a.pesoAristasVertices(1, 0) + "");
         //a.modificarArista(0, 1, 2.0,3.0);
         //print(a.pesoAristasVertices(0,1)+"");
         //print(a.pesoAristasVertices(1,0)+"");
