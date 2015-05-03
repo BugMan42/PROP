ecf8d3a0c35b9ca34a4a2797f684cb1a0e8428f2
BugMan42 <guillem812@gmail.com>
2015-05-03 21:04:55 +0200

    congresista y congreso limpios

--- src/Domini/Congresista.java
+++ src/Domini/Congresista.java
@@ -1,7 +1,5 @@
 package Domini;
-/**
- * Clase Congresista
- */
+
 public class Congresista {
 
     private class EdadNoValida extends Exception {
@@ -14,7 +12,6 @@ public class Congresista {
      * Dni es un Dni valido
      * Edad es un entero > 0
      * Los otros parametros son Strings
-     *
      */
     private Dni Dni;
     private String Nombre;
@@ -22,13 +19,9 @@ public class Congresista {
     private int Edad;
     private String Ciudad;
     private String Estado;
-
     private String Partido;
+
     //Creadoras
-    /** Pre: edad valida
-     *  Post: Se crea un Congresista con sus atributos
-     *  Si edad no valida Excepcion
-     */
     public Congresista (Dni dni, String nombre, String apellido, int edad, String ciudad, String estado, String partido) throws Exception {
         if (!EdadValida(edad)) throw new EdadNoValida(edad);
         Dni = dni;
@@ -39,13 +32,6 @@ public class Congresista {
         Estado = estado;
         Partido = partido;
     }
-
-    /**
-     * Creadora copiadora de un Congresista
-     * Se crea un Congresista con los atributos
-     * del Congresista c
-     * es una copia en profundidad(deep copy)
-     */
     public Congresista(Congresista c) {
         Dni = c.obtDni();
         Nombre = c.obtNombre();
@@ -56,55 +42,28 @@ public class Congresista {
         Partido = c.obtPartido();
     }
     //Modificadoras
-    /**
-     * Modifica el Dni
-     */
     public void modDni(Dni dni) {
         Dni = dni;
     }
-    /**
-     * Modifica el Nombre
-     */
     public void modNombre(String nombre) {
         Nombre = nombre;
     }
-    /**
-     * Modifica el Apellido
-     */
     public void modApellido(String apellido) {
         Apellido = apellido;
     }
-    /**
-     * Pre: edad valida
-     * Post: Modifica la edad
-     * Si no excepcion
-     */
     public void modEdad(int edad) throws Exception {
         if (!EdadValida(edad)) throw new EdadNoValida(edad);
         else Edad = edad;
     }
-    /**
-     * Modifica la Ciudad
-     */
     public void modCiudad(String ciudad) {
         Ciudad = ciudad;
     }
-    /**
-     * Modifica el Estado
-     */
     public void modEstado(String estado) {
         Estado = estado;
     }
-    /**
-     * Modifica el Partido
-     */
     public void modPartido(String partido) {
         Partido = partido;
     }
-    /**
-     * Modifica todos sus atributos
-     * Si edad no es valida lanza una excepcion
-     */
     public void mod(Dni dni, String nombre, String apellido, int edad, String ciudad, String estado, String partido) throws Exception {
         if (!EdadValida(edad)) throw new EdadNoValida(edad);
         Dni = dni;
@@ -118,67 +77,36 @@ public class Congresista {
 
     //Consultoras
 
-    /**
-     * Retorna el Dni del Congresista
-     */
     public Dni obtDni() {
         return Dni;
     }
-    /**
-     * Retorna el Nombre del Congresista
-     */
     public String obtNombre() {
         return Nombre;
     }
-    /**
-     * Retorna el Apellido del Congresista
-     */
     public String obtApellido() {
         return Apellido;
     }
-    /**
-     * Retorna la Edad del Congresista
-     */
     public int obtEdad() {
         return Edad;
     }
-    /**
-     * Retorna la Ciudad del Congresista
-     */
     public String obtCiudad() {
         return Ciudad;
     }
-    /**
-     * Retorna el Estado del Congresista
-     */
     public String obtEstado() {
         return Estado;
     }
-    /**
-     * Retorna el Partido del Congresista
-     */
     public String obtPartido() {
         return Partido;
     }
-    /**
-     * Retorna el Identificador del Congresista
-     * retorna el dni en string
-     */
     public String ID() {
         return Dni.toString();
     }
-    /**
-     * Retorna un String conteniendo todos sus
-     * atributos
-     */
     public String toString() {
         return Dni+" "+Nombre+" "+Apellido+" "+Edad+" "+Ciudad+" "+Estado+" "+Partido;
     }
 
-    /**
-     * Valida la edad: Comprueba si
-     * el parametro n es un entero > 0
-     */
+     //Valida la edad: Comprueba si
+     //el parametro n es un entero > 0
     private static boolean EdadValida(int n) {
         return n > 0;
     }
--- src/Domini/Congreso.java
+++ src/Domini/Congreso.java
@@ -16,61 +16,14 @@ public class Congreso {
 
     private TST<Congresista> tst;
 
-    /**
-     * Creadora Congreso
-     */
     public Congreso() {
         tst = new TST<Congresista>();
     }
 
-    /**
-     * Retorna el numero de Congresistas
-     */
-    public int size() {
-        return tst.size();
-    }
-
-    /**
-     * Retorna un booleano que indica si
-     * el congreso esta vacio
-     */
-    public boolean esVacio() {
-        return tst.esVacio();
-    }
-
-    /**
-     * Agrega el Congresista c al Congreso
-     */
     public void agregarCongresista(Congresista c) throws Exception {
         tst.insertar(c.ID(),c);
     }
 
-    /**
-     * Devuelve el listado de dnis del Congreso
-     */
-    public ArrayList<String> obtenerListaID() {
-        return tst.consultarClaves();
-    }
-
-    /**
-     * devuelve el listado de Congresistas del Congreso
-     */
-    public List<Congresista> obtenerCongreso() {
-        return tst.consultarObjetos();
-    }
-
-    /**
-     * Retorna si existe en el Congreso
-     * un Congresista con dni igual al parametro
-     */
-    public boolean contieneCongresista(Dni dni) {
-        return tst.existe(dni.toString());
-    }
-
-    /**
-     * Elimina el Congresista del Congreso con
-     * Dni=dni en el caso que exista
-     */
     public void eliminarCongresista(Dni dni) throws Exception {
         try {
             tst.borrar(dni.toString());
@@ -80,9 +33,6 @@ public class Congreso {
         }
     }
 
-    /**
-     * Elimina todo el Congreso
-     */
     public void eliminarCongreso() {
         tst.vaciar();
     }
@@ -91,83 +41,6 @@ public class Congreso {
     //########MODIFICADORAS###############
     //####################################
 
-    /**
-     * Modifica el nombre del Congresista con Dni=dni
-     * si existe
-     */
-    public void modNombreCongresista(Dni dni,String nombre) throws Exception {
-        try {
-            tst.obtener(dni.toString()).modNombre(nombre);
-        }
-        catch (Exception a) {
-            throw new NoExisteC(dni.toString());
-        }
-    }
-    /**
-     * Modifica el apellido del Congresista con Dni=dni
-     * si existe
-     */
-    public void modApellidoCongresista(Dni dni,String apellido) throws Exception {
-        try {
-            tst.obtener(dni.toString()).modApellido(apellido);
-        }
-        catch (Exception a) {
-            throw new NoExisteC(dni.toString());
-        }
-    }
-    /**
-     * Modifica la edad del Congresista con Dni=dni
-     * si existe
-     */
-    public void modEdadCongresista(Dni dni,int edad) throws Exception {
-        try {
-            tst.obtener(dni.toString()).modEdad(edad);
-        }
-        catch (Exception a) {
-            throw new NoExisteC(dni.toString());
-        }
-    }
-    /**
-     * Modifica la ciudad del Congresista con Dni=dni
-     * si existe
-     */
-    public void modCiudadCongresista(Dni dni,String ciudad) throws Exception {
-        try {
-            tst.obtener(dni.toString()).modCiudad(ciudad);
-        }
-        catch (Exception a) {
-            throw new NoExisteC(dni.toString());
-        }
-    }
-    /**
-     * Modifica el estado del Congresista con Dni=dni
-     * si existe
-     */
-    public void modEstadoCongresista(Dni dni,String estado) throws Exception {
-        try {
-            tst.obtener(dni.toString()).modEstado(estado);
-        }
-        catch (Exception a) {
-            throw new NoExisteC(dni.toString());
-        }
-    }
-    /**
-     * Modifica el partido del Congresista con Dni=dni
-     * si existe
-     */
-    public void modPartidoCongresista(Dni dni,String partido) throws Exception {
-        try {
-            tst.obtener(dni.toString()).modPartido(partido);
-        }
-        catch (Exception a) {
-            throw new NoExisteC(dni.toString());
-        }
-    }
-    /**
-     * Modifica el Dni del Congresista con Dni=dni
-     * si existe y solo en el caso que no exista otro
-     * Congresista con Dni=DniNuevo
-     */
     public void modDniCongresista(Dni dni, Dni dniNuevo) throws Exception {
         if (!dni.equals(dniNuevo)) {
             try {
@@ -177,14 +50,64 @@ public class Congreso {
             catch(Exception a) {
                 if (a.getClass().equals(KeyNotExistsTST.class)) throw new NoExisteC(dni.toString());
                 if (a.getClass().equals(KeyAlreadyExistsTST.class)) throw new ExisteC(dniNuevo.toString());
-
             }
         }
     }
-    /**
-     * Modifica Todos los atributos del Congresista
-     * de una vez
-     */
+
+    public void modNombreCongresista(Dni dni,String nombre) throws Exception {
+        try {
+            tst.obtener(dni.toString()).modNombre(nombre);
+        }
+        catch (Exception a) {
+            throw new NoExisteC(dni.toString());
+        }
+    }
+
+    public void modApellidoCongresista(Dni dni,String apellido) throws Exception {
+        try {
+            tst.obtener(dni.toString()).modApellido(apellido);
+        }
+        catch (Exception a) {
+            throw new NoExisteC(dni.toString());
+        }
+    }
+
+    public void modEdadCongresista(Dni dni,int edad) throws Exception {
+        try {
+            tst.obtener(dni.toString()).modEdad(edad);
+        }
+        catch (Exception a) {
+            throw new NoExisteC(dni.toString());
+        }
+    }
+
+    public void modCiudadCongresista(Dni dni,String ciudad) throws Exception {
+        try {
+            tst.obtener(dni.toString()).modCiudad(ciudad);
+        }
+        catch (Exception a) {
+            throw new NoExisteC(dni.toString());
+        }
+    }
+
+    public void modEstadoCongresista(Dni dni,String estado) throws Exception {
+        try {
+            tst.obtener(dni.toString()).modEstado(estado);
+        }
+        catch (Exception a) {
+            throw new NoExisteC(dni.toString());
+        }
+    }
+
+    public void modPartidoCongresista(Dni dni,String partido) throws Exception {
+        try {
+            tst.obtener(dni.toString()).modPartido(partido);
+        }
+        catch (Exception a) {
+            throw new NoExisteC(dni.toString());
+        }
+    }
+
     public void modCongresista(Dni dni,Dni dniNuevo , String nombre, String apellido,
                                      int edad, String ciudad, String estado, String partido) throws  Exception{
         try {
@@ -200,9 +123,6 @@ public class Congreso {
         }
     }
 
-    /**
-     * Retorna la instancia del Congresista con Dni=dni
-     */
     public Congresista consultarCongresista(Dni dni) throws Exception {
         try {
             return tst.obtener(dni.toString());
@@ -211,9 +131,7 @@ public class Congreso {
             throw new NoExisteC(dni.toString());
         }
     }
-    /**
-     * Retorna una copia del Congresista con Dni=dni
-     */
+
     public Congresista copiaConsultarCongresista(Dni dni) throws Exception {
         try {
             Congresista aux = new Congresista(tst.obtener(dni.toString()));
@@ -223,16 +141,30 @@ public class Congreso {
             throw new NoExisteC(dni.toString());
         }
     }
-    /**
-     * Retorna un String que contiene los dnis de todo
-     * el congreso
-     */
+
+    public int size() {
+        return tst.size();
+    }
+
+    public boolean esVacio() {
+        return tst.esVacio();
+    }
+
+    public ArrayList<String> obtenerListaID() {
+        return tst.consultarClaves();
+    }
+
+    public List<Congresista> obtenerCongreso() {
+        return tst.consultarObjetos();
+    }
+
+    public boolean contieneCongresista(Dni dni) {
+        return tst.existe(dni.toString());
+    }
+
     public String toString() {
         String aux = tst.toString();
         return aux;
-    }
-    private void print(String d) {
-        System.out.println(d);
     }
 
 }
