8d6222769a6dc525b232cdd362f1719707784d85
FALC <falc@null.net>
2015-05-04 12:45:29 +0200

    Cambios de implementación

--- src/Domini/DriverVotacion.java
+++ src/Domini/DriverVotacion.java
@@ -9,12 +9,9 @@ import java.util.Scanner;
 public class DriverVotacion {
 
     final static String text1 = "***DRIVER DE VOTACIÓN***";
-    final static String opcion1 = "***1.-ALTA DE VOTO";
-    final static String opcion2 = "***2.-CONSULTA DE VOTO";
-    final static String opcion3 = "***3.-MODIFICACIÓN DE VOTO";
-    final static String opcion4 = "***4.-BAJA DE VOTO";
-    final static String opcion5 = "***5.-AYUDA";
-    final static String opcion6 = "***6.-SALIR";
+    final static String opcion1 = "***1.-CONSULTA DE VOTACIÓN";
+    final static String opcion5 = "***2.-AYUDA";
+    final static String opcion6 = "***3.-SALIR";
 
     private static Scanner user_input = new Scanner( System.in );
     private static Votacion vt;
@@ -33,110 +30,23 @@ public class DriverVotacion {
             opcion = Integer.valueOf(user_input.next());
             decide(opcion);
         }
-        while (opcion != 6);
-    }
-
-    public static void alta() {
-        System.out.println("Escribe el tipo de voto a introducir:");
-        System.out.println("(1: Blanco, 2: Abstencion, 3: Positivo, 4: Negativo o 5: Nulo)");
-        String s1 = user_input.next();
-        Class c;
-        Voto v;
-        try {
-            c = Class.forName("Domini." + s1);
-            v = (Voto) c.newInstance();
-        } catch (Exception e) {
-            //e.printStackTrace();
-            System.out.println("Tipo de voto no válido, operación cancelada.");
-            return;
-        }
-
-
-        System.out.println("Escribe el DNI del congresista votante:");
-        String s2 = user_input.next();
-        try {
-            v.mod_dni(s2);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("DNI incorrecto, operación cancelada");
-            return;
-        }
-
-        try {
-            vt.añadir_voto(v);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("DNI ya existente, operación cancelada");
-        }
+        while (opcion != 3);
     }
 
     public static void consulta()
     {
-        System.out.println("Introduce el DNI del voto a buscar:");
-        String v2 = null;
-        try {
-            v2 = vt.consultar_voto(user_input.next());
-            System.out.println(v2);
-        } catch (Exception e) {
-            System.out.println("DNI introducido es incorrecto, operación cancelada.");
-        }
-    }
-
-    public static void baja()
-    {
-        System.out.println("Introduce el DNI del voto a borrar:");
-        try {
-            vt.eliminar_voto(user_input.next());
-        } catch (Exception e) {
-            System.out.println("DNI introducido es incorrecto, operación cancelada.");
-        }
-    }
-
-    public static void modificacion() {
-        System.out.println("Introduce el DNI del voto a modificar:");
-        String s1 = user_input.next();
-        try {
-            vt.eliminar_voto(s1);
-        } catch (Exception e) {
-            System.out.println("DNI introducido es incorrecto, operación cancelada.");
-            return;
-        }
-
-        System.out.println("Introduce el tipo de voto:");
-        System.out.println("(Nulo, Blanco, Abstencion, Positivo o Negativo)");
-        String s2 = user_input.next();
-        Class c;
-        Voto v;
-        try {
-            c = Class.forName("Domini." + s2);
-            v = (Voto) c.newInstance();
-        } catch (Exception e) {
-            //e.printStackTrace();
-            System.out.println("Tipo de voto no válido, operación cancelada.");
-            return;
-        }
-
-        try {
-            vt.añadir_voto(s1, v);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("DNI ya existe, operación cancelada");
-        }
-
+        vt.toString();
     }
 
     public static void ayuda() {
         System.out.println("Driver del sistema de votaciones");
-        System.out.println("En el menú, seleccione la opción que desee. Para salir, utilice la opción 6.");
+        System.out.println("En el menú, seleccione la opción que desee. Para salir, utilice la opción 3.");
     }
 
     public static void print_menu()
     {
         System.out.println("\n"+text1);
         System.out.println(opcion1);
-        System.out.println(opcion2);
-        System.out.println(opcion3);
-        System.out.println(opcion4);
         System.out.println(opcion5);
         System.out.println(opcion6);
         System.out.println("\nEscribe el número de la opción que quieras:");
@@ -146,21 +56,13 @@ public class DriverVotacion {
         switch (opt)
         {
             case 1:
-                alta();
-                break;
-            case 2:
                 consulta();
                 break;
-            case 3:
-                modificacion();
-                break;
-            case 4:
-                baja();
-                break;
-            case 5:
+            case 2:
                 ayuda();
                 break;
             default:
+                System.out.println("Opción incorrecta, vuelve a introducir un número");
                 break;
         }
     }
--- src/Domini/Votacion.java
+++ src/Domini/Votacion.java
@@ -7,25 +7,13 @@ package Domini;
  */
 public class Votacion extends Evento {
 
-    //Estructura de datos que ordena los votos según el DNI que los identifica
-    private TST<Voto> votos = new TST<Voto>();
-
     //Creadora con pará,etros de Votación
     public Votacion(String name, String date, int importance) throws Exception{
         //Llamada a la creadora con parámetros de Evento
         super(name, date, importance);
     }
 
-    // Método de inserción de votos mediante un String diferente al DNI del Voto
-    public void añadir_voto(String id, Voto v) throws Exception {
-        votos.insertar(id, v);
-    }
-
-    //Método de inserción de votos teniendo como clave para insertar el DNI del Voto
-    public void añadir_voto(Voto v) throws Exception {
-        votos.insertar(v.obt_dni(), v);
-    }
-
+/*
     //Siendo id el DNI de v, este método permite canviar el tipo de Voto de un Congresista para una votación
     public void modificar_voto(String id, Voto v) throws Exception {
         votos.borrar(id);
@@ -46,7 +34,7 @@ public class Votacion extends Evento {
     public Voto obt_voto(String id) throws Exception {
         return votos.obtener(id);
     }
-
+*/
     //Conversor de Votación a String
     public String toString() {
         return "Votacion " + nombre + " " + fecha.ConsultarFecha() + " " + Integer.toString(importancia);
