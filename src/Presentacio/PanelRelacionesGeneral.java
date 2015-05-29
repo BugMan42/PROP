8f29daeb9c359dd7200ada2b5eb622de1cb36e07
jose <sam92.sp@gmail.com>
2015-05-29 22:01:07 +0200

    vista de todas las relaciones

--- src/Presentacio/PanelRelacionesGeneral.java
+++ src/Presentacio/PanelRelacionesGeneral.java
@@ -5,10 +5,12 @@ import javax.swing.*;
 /**
  * Created by jose on 29/05/15.
  */
-public class PanelRelacionesGeneral extends JPanel {
+public class PanelRelacionesGeneral extends Panel3Listas {
 
     public PanelRelacionesGeneral(){
-
+        pl1.titulo.setText("Congresistas");
+        pl2.titulo.setText("Eventos");
+        pl3.titulo.setText("Relaciones");
     }
 
 }
